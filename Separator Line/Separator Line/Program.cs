b800e6e734fe22ec3d266527c14458ed22aa2100
riadHossain <riadofficial.me@gmail.com>
2021-07-09 00:58:47 +0600

    Revert "Revert "Revert "new file added"""
    This reverts commit e5851987796af8263a7665eb217faaf9b288c780.

--- Separator Line/Separator Line/Program.cs
+++ Separator Line/Separator Line/Program.cs
@@ -14,7 +14,6 @@ namespace Separator_Line
         [STAThread]
         static void Main()
         {
-            Console.WriteLine("Test Tortoise");
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmDemo());
